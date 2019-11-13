299bb373a98284b8ad55679e2522273f7be9c82c
Vitor Castro <vitorhugocastro95@gmail.com>
2019-11-13 13:17:23 +0000

    [I Am Rich] Scaffolding a Flutter App

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our Flutter apps.
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Center(
+              child: Text(
+                'I Am Rich',
+              ),
+            ),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+            ),
+          ),
+        ),
+      ),
+    );
